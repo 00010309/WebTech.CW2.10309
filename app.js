cc4d016c94f67c7c8984f3492f4e7a7e7685e62d
00010309 <wiut00010309@gmail.com>
2021-04-05 16:48:11 +0500

    Running the server

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', (req, res)=>{
     res.send('Hello')
 })
 
-app.listen(8000, err=>{
-    err ? console.log(err) : console.log("The server is running on the port 8000")
+app.listen(1000, err=>{
+    err ? console.log(err) : console.log("The server is running on the port 1000")
 })
